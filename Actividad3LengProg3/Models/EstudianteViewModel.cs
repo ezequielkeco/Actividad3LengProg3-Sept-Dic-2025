ac84a249693d2a28aac95918fa32797a3b318b3c
Ezequiel Santos <ezequielsantosg@gmail.com>
2025-10-02 15:56:31 -0400

    Parámetros del modelo EstudianteViewModel
    Definición del modelo de datos con sus validaciones.

---
+++ Actividad3LengProg3/Models/EstudianteViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Actividad3LengProg3.Models
+{
+    public class EstudianteViewModel
+    {
+        [Required, StringLength(100)]
+        [Display(Name = "Nombre completo")]
+        public string NombreCompleto { get; set; }
+
+        [Required, StringLength(15, MinimumLength = 6)]
+        public string Matricula { get; set; }
+
+        [Required]
+        public string Carrera { get; set; }
+
+        [Required]
+        public string Recinto { get; set; }
+
+        [Required, EmailAddress]
+        [Display(Name = "Correo institucional")]
+        public string Correo { get; set; }
+
+        [Phone, MinLength(10)]
+        public string Celular { get; set; }
+
+        [Phone, MinLength(10)]
+        public string Telefono { get; set; }
+
+        [Required, StringLength(200)]
+        public string Direccion { get; set; }
+
+        [Required, DataType(DataType.Date)]
+        [Display(Name = "Fecha de nacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required]
+        public string Genero { get; set; }
+
+        [Required]
+        public string Turno { get; set; }
+
+        [Display(Name = "¿Está becado?")]
+        public bool EstaBecado { get; set; }
+
+        [Range(0, 100)]
+        [Display(Name = "Porcentaje de beca")]
+        public int? PorcentajeBeca { get; set; }
+    }
+}
+
